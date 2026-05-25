67b89200e3d0083495bf51cbf8a91949e0b0a48d
AbhiralJain07 <jainabhiral7@gmail.com>
2026-05-26 00:26:01 +0530

    connected frontend with production backend

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://evolvit-backend1.onrender.com/api',
 });
 
 // Har request se pehle token automatically add ho jaaye
