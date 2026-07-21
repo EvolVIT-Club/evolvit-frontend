bd03a89d80973bf6efc4c5f2c54892ad08fb8000
AbhiralJain07 <jainabhiral7@gmail.com>
2026-07-21 20:19:49 +0530

    refurnished

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://evolvit-backend-5ior.onrender.com/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://evolvit-backend-ten.vercel.app/',
 });
 
 // Har request se pehle token automatically add ho jaaye
